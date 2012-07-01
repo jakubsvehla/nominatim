8affb2ccf2ca91857a50e65499bd55f8c806a4e6
Jakub Svehla <jakub.svehla@gmail.com>
2012-07-01 13:09:49 +0200

    reset the configuration before each spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  config.before do
+    Nominatim.config.reset!
+  end
 end
 
 def a_get(path)
