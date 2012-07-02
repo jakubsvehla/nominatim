670d43d885f184120531d0caf82e5e574474eb07
Jakub Svehla <jakub.svehla@gmail.com>
2012-07-02 21:52:35 +0200

    Add docs to the Place class.

--- lib/nominatim/place.rb
+++ lib/nominatim/place.rb
@@ -7,27 +7,45 @@ module Nominatim
       @attrs = attrs
     end
 
+    # Returns display name
+    #
+    # @return [String]
     def display_name
       @display_name ||= @attrs[:display_name]
     end
 
+    # Return a class
+    #
+    # @return [String]
     def class
       @class ||= @attrs[:class]
     end
 
+    # Return a type
+    #
+    # @return [String]
     def type
       @type ||= @attrs[:type]
     end
 
+    # Return an address
+    #
+    # @return [Nominatim::Address]
     def address
       @address ||= Nominatim::Address.new(@attrs[:address]) unless @attrs[:address].nil?
     end
 
+    # Return a latitude
+    #
+    # @return [Float]
     def lat
       point.lat
     end
     alias latitude lat
 
+    # Return a longitude
+    #
+    # @return [Float]
     def lon
       point.lon
     end
@@ -42,14 +60,23 @@ module Nominatim
       @polygonpoints ||= @attrs[:polygonpoints]
     end
 
+    # Return a place id
+    #
+    # @return [Integer]
     def place_id
       @place_id ||= @attrs[:place_id].to_i if @attrs[:place_id]
     end
 
+    # Return an OSM id
+    #
+    # @return [Integer]
     def osm_id
       @osm_id ||= @attrs[:osm_id].to_i if @attrs[:osm_id]
     end
 
+    # Return an OSM type
+    #
+    # @return [String]
     def osm_type
       @osm_type ||= @attrs[:osm_type]
     end
