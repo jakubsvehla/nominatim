35dcb6b2d4c2a01350bb31473eeb01cfc13e467e
Łukasz Jachymczyk <lukasz.jachymczyk@sology.pl>
2012-09-10 14:29:35 +0200

    Add reverse geocoding support.

---
+++ lib/nominatim/reverse.rb
@@ -0,0 +1,42 @@
+module Nominatim
+  class Reverse < Client
+    attr_reader :criteria
+
+    def initialize
+      @criteria = {}
+    end
+
+    # Returns search result.
+    def fetch
+      Nominatim::Place.new(get('/reverse', @criteria).body)
+    end
+
+    # Latitude string to search for.
+    #
+    # @param lat [String] Latitude
+    # @return [Nominatim::Reverse]
+    def lat(lat)
+      @criteria[:lat] = lat
+      self
+    end
+
+    # Longitude string to search for.
+    #
+    # @param lon [String] Longitude
+    # @return [Nominatim::Reverse]
+    def lon(lon)
+      @criteria[:lon] = lon
+      self
+    end
+
+    # Include a breakdown of the address into elements.
+    #
+    # @param bool [true, false]
+    # @return [Nominatim::Reverse]
+    def address_details(bool)
+      @criteria[:addressdetails] = bool ? 1 : 0
+      self
+    end
+
+  end
+end
